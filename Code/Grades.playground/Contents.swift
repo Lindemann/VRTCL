33eec759fcf6e527830032c2b42e8e338d213c69
Lindemann <Lindemann@Judiths-MacBook-Pro.local>
2015-05-18 06:14:35 +0200

    ...

--- Code/Grades.playground/Contents.swift
+++ Code/Grades.playground/Contents.swift
@@ -8,7 +8,7 @@ struct Grade {
 }
 
 struct Color {
-    static let BLUE = UIColor(hue:0.47, saturation:1, brightness:0.97, alpha:1)
+    static let BLUE = UIColor(red:0, green:0.97, blue:0.77, alpha:1)
 }
 
 enum System {
@@ -21,3 +21,50 @@ enum System {
 
 let grade = Grade(system: .UIAA, value: "5", color: Color.BLUE, display: true)
 Grade(system: .UIAA, value: "5-/5+", color: nil, display: false)
+
+struct Climb {
+    var style: Style
+    var grade: Grade
+}
+
+enum Style {
+    case Flash
+    case OnSight
+    case RedPoint
+    case Attempt
+    case Toprope
+}
+
+enum Kind {
+    case SportClimbing
+    case Bouldering
+}
+
+struct Session {
+    let kind: Kind
+    var climbs: Array<Climb>
+    var feeling: Feeling
+    var location: Location
+    var duration: Double
+}
+
+enum Feeling {
+    case Fabolous
+    case OK
+    case Exhausted
+    case Hideous
+}
+
+struct Location {
+    var name: String
+    var geoLocation: Int
+}
+
+struct Grades {
+    static let UIAA: [Grade] = [
+        Grade(system: .UIAA, value: "", color: nil, display: true),
+        Grade(system: .UIAA, value: "", color: nil, display: true)
+    ]
+}
+
+println(Grades.UIAA)
