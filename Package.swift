6ee44cdc3c6df3e924b3bf55c28e9895e0068bb5
Judith Lindemann <judith@evenly.io>
2017-05-18 02:41:15 +0200

    Added initial Docker and Kitura setup

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "vrtcl-api",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
+    ])
